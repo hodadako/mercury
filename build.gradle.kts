f22c998d00b5fb9e6dd739014c93c15ace32e721
hodako <kbry12@naver.com>
2025-06-20 15:21:28 +0900

    chore : change gradle language to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    repositories {
+        mavenLocal()
+        google()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    }
+}
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+    google()
+}

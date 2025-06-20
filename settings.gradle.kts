f666ea5746af3e0ab13b0dcd6043821914015af3
hodako <kbry12@naver.com>
2025-06-20 16:51:23 +0900

    chore : changed settings.gradle to kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+rootProject.name = "mercury"
+include("core")
+
+pluginManagement {
+    buildscript {
+        repositories {
+            gradlePluginPortal()
+        }
+    }
+
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}

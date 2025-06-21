c178dd454cbdd3fe3f58fbb1145ae561ba076158
hodako <kbry12@naver.com>
2025-06-21 21:16:01 +0900

    feat : introduced EncodingDetector to detect encoding from bytes arrays

---
+++ core/src/main/java/com/mercury/encoding/EncodingDetector.java
@@ -0,0 +1,8 @@
+package com.mercury.encoding;
+/*
+ * Copyright (c) 2025 mercury contributors
+ * This program is made available under the terms of the Apache License.
+ */
+public interface EncodingDetector {
+	String detectEncoding(byte[] data);
+}

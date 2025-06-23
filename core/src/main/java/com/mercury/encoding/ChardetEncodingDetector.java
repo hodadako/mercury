b42bd68f44e02870bb32941694e8e5521b93197f
hodako <kbry12@naver.com>
2025-06-23 18:27:56 +0900

    feat : added new encoding detector implementation using juniversalchardet

---
+++ core/src/main/java/com/mercury/encoding/ChardetEncodingDetector.java
@@ -0,0 +1,25 @@
+package com.mercury.encoding;
+
+import org.mozilla.universalchardet.UniversalDetector;
+
+/*
+ * Copyright (c) 2025 mercury contributors
+ * This program is made available under the terms of the Apache License.
+ */
+public class ChardetEncodingDetector implements EncodingDetector {
+
+	@Override
+	public String detectEncoding(byte[] data) {
+		if (data.length == 0)
+			return "";
+
+		UniversalDetector detector = new UniversalDetector(null);
+		detector.handleData(data, 0, data.length);
+		detector.dataEnd();
+
+		String encoding = detector.getDetectedCharset();
+		detector.reset();
+
+		return encoding != null ? encoding : "";
+	}
+}

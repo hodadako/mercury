c407a3b010af3fb62636ec6f284b5b57116f5eb0
hodako <kbry12@naver.com>
2025-07-13 23:26:56 +0900

    feat : add skeleton for DNAEncoder

---
+++ core/src/main/java/com/mercury/dna/DNAEncoder.java
@@ -0,0 +1,9 @@
+package com.mercury.dna;
+
+/*
+ * Copyright (c) 2025 mercury contributors
+ * This program is made available under the terms of the Apache License.
+ */
+public interface DNAEncoder {
+	String encode(String data);
+}

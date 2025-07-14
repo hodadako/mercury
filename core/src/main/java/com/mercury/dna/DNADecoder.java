0a6c6e1967b171c879ba9ecede5430640807f15a
hodako <kbry12@naver.com>
2025-07-14 23:39:11 +0900

    feat : add DNADecoder interface

---
+++ core/src/main/java/com/mercury/dna/DNADecoder.java
@@ -0,0 +1,9 @@
+package com.mercury.dna;
+
+/*
+ * Copyright (c) 2025 mercury contributors
+ * This program is made available under the terms of the Apache License.
+ */
+public interface DNADecoder {
+	String decode(String data);
+} 

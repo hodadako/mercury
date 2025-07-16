1ed744f3026179be265a8a6ea8966bfe556332b3
hodako <kbry12@naver.com>
2025-07-16 16:31:46 +0900

    style : changed line break in DNAConversionConstants

--- core/src/main/java/com/mercury/dna/DNAConversionConstants.java
+++ core/src/main/java/com/mercury/dna/DNAConversionConstants.java
@@ -11,10 +11,7 @@ public final class DNAConversionConstants {
 	public static final String START_CODON = "ATG";
 	public static final List<String> STOP_CODONS = List.of("TAA", "TAG", "TGA");
 	public static final Map<Integer, Character> TWO_BITS_TO_BASE_MAP = Map.of(
-		0b00, 'A', 
-		0b01, 'C',
-		0b10, 'G',
-		0b11, 'T'
+		0b00, 'A', 0b01, 'C', 0b10, 'G', 0b11, 'T'
 	);
 
 	/**

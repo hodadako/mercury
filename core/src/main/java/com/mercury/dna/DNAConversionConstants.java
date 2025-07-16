0139f60676bb2e138ecf2f028ebb5f63acb1534b
hodako <kbry12@naver.com>
2025-07-16 17:11:13 +0900

    style : changed line break

--- core/src/main/java/com/mercury/dna/DNAConversionConstants.java
+++ core/src/main/java/com/mercury/dna/DNAConversionConstants.java
@@ -11,13 +11,16 @@ public final class DNAConversionConstants {
 	public static final String START_CODON = "ATG";
 	public static final List<String> STOP_CODONS = List.of("TAA", "TAG", "TGA");
 	public static final Map<Integer, Character> TWO_BITS_TO_BASE_MAP = Map.of(
-		0b00, 'A', 0b01, 'C', 0b10, 'G', 0b11, 'T'
+		0b00, 'A', 
+		0b01, 'C',
+		0b10, 'G',
+		0b11, 'T'
 	);
 
 	/**
 	 * Maps DNA base characters to their 2-bit integer representation.
 	 */
-	public static final Map<Character, Integer> BASE_TO_BITS_MAP = Map.of(
+	public static final Map<Character, Integer> BASE_TO_TWO_BITS_MAP = Map.of(
 		'A', 0b00,
 		'C', 0b01,
 		'G', 0b10,

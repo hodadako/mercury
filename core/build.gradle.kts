ecd359d41be26c9ecc7ebab268d94c7472f6c26b
hodako <kbry12@naver.com>
2025-07-25 12:06:18 +0900

    chore : fix version number in publishing

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -32,7 +32,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.hodadako",
         artifactId = "mercury-core",
-        version = "0.1.0"
+        version = "0.1.2"
     )
 
     pom {
